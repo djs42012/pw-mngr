243ea98ab8a3a827359085ace8e0ffa2afe6aa89
david <github@djs.gg>
2022-03-03 12:42:09 -0500

    auto load passwords

--- client/components/TotalsDisplay.jsx
+++ client/components/TotalsDisplay.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-//import { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
 import LabeledText from './LabeledText.jsx';
-import * as actions from '../actions/actions';
 
 
-// useEffect(() => {
-//   actions.loadPasswords()
-// }, []);
 
 
-const TotalsDisplay = (props) => (
+const TotalsDisplay = props => {
+  
+  useEffect(() => {
+    props.loadPasswords()
+  }, [])
+
+  return (
   <div className="innerbox" id="totals">
     <h3>Totals Display</h3>
     <div>
       <LabeledText label="Total Passwords" text={props.totalPasswords}/>
     </div>
-    <button id='sync' className="secondary" onClick={props.loadPasswords}>Load Passwords</button>
   </div>
-);
+  );
+};
 
 export default TotalsDisplay;
