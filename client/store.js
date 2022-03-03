4177dd62ac0fc87467d6b0757f2df0a843dc0b9c
david <github@djs.gg>
2022-03-03 09:03:58 -0500

    redux reimplemented

--- client/store.js
+++ client/store.js
@@ -1,13 +1,11 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index.js';
-//import thunk from 'redux-thunk';
 import { loadPasswords } from './actions/actions';
 
 const store = createStore(
   reducers,
   composeWithDevTools(),
-  //composeWithDevTools(applyMiddleware(thunk)),
 );
 store.dispatch(loadPasswords());
 
