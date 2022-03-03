9f5547e6dbbba8e5c4838baca9f5382dac60a6dd
david <github@djs.gg>
2022-03-03 13:19:01 -0500

    load all password fields

--- client/components/Password.jsx
+++ client/components/Password.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import LabeledText from './LabeledText.jsx';
 
-const Password= () => (
+
+const Password= (props) => (
   <div className="passwordBox">
-    <h3>Password</h3>
+    <h3>{props.account}</h3>
     <div>
-      <LabeledText label="Prop 1" text='text' />
-      <LabeledText label="Prop 2" text='text' />
-      <LabeledText label="Prop 3" text='text' />
+      <LabeledText label="pwid" text={props.pwid} />
+      <LabeledText label="account" text={props.account} />
+      <LabeledText label="username" text={props.username} />
+      <LabeledText label="alias" text={props.alias} />
+      <LabeledText label="password" text={props.password} />
+      <LabeledText label="uri" text={props.uri} />
+      <LabeledText label="notes" text={props.notes} />
     </div>
     <div className="flex">
-      <button className='action1'>Action</button>
-      <button className='action2'>Action</button>
+      <button className='action1'>Delete</button>
+      <button className='action2'>Update</button>
     </div>
   </div>
 );
